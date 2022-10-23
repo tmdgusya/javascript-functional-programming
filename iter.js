0f73dd7fe7c826e3b169dd435d541c615a092e52
tmdgusya <tmdgusya@naver.com>
2022-10-23 15:49:10 +0900

    iterator and iterable

---
+++ iter.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3];
+for (const a of arr) console.log(a);
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
+
+const set = new Set([1, 2, 3]);
+for (const a of set) log(a);
+
